79bc9f58ccea5f3d3b9ec4baf878620fbf2614a2
Fernando Tanaka <fernando.ty.tanaka@gmail.com>
2015-04-14 20:28:38 -0300

    changed STACK_end and STACK_pop

--- 2015-1/grafos-MAC0328/ep1/stack.h
+++ 2015-1/grafos-MAC0328/ep1/stack.h
@@ -13,8 +13,8 @@ stack *STACK_new ( int );
 
 void STACK_push ( stack *, int, int );
 
-stack *STACK_pop ( stack * );
+arc *STACK_pop ( stack * );
 
-void STACK_end ( stack * );
+stack *STACK_end ( stack * );
 
 void STACK_print ( stack * );
