3a1f99b468b0233b43b22afbe369046c0046b78a
Fernando Tanaka <fernando.ty.tanaka@gmail.com>
2015-04-15 16:24:45 -0300

    last test

--- 2015-1/grafos-MAC0328/ep1/graphT.c
+++ 2015-1/grafos-MAC0328/ep1/graphT.c
@@ -10,8 +10,14 @@ int main ( void ) {
    grafo = NULL;
    arco  = NULL;
 
+   printf ( "# TESTE 1 - GRAPH_input\n" );
    grafo = GRAPH_input ();
    GRAPH_print ( grafo );
+   printf ( "# FIM TESTE 1\n" );
+
+   printf ( "# ULTIMO TESTE - GRAPH_end ( grafo )\n" );
+   grafo = GRAPH_end ( grafo );
+   printf ( "# FIM ULTIMO TESTE\n" );
 
    return 0;
 }
