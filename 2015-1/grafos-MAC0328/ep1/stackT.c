89f7bc0cbe806e07aa327ea6e8e333ab8e0fa3c6
Fernando Tanaka <fernando.ty.tanaka@gmail.com>
2015-04-14 23:08:41 -0300

    added some tests

--- 2015-1/grafos-MAC0328/ep1/stackT.c
+++ 2015-1/grafos-MAC0328/ep1/stackT.c
@@ -6,6 +6,7 @@
 int main ( void ) {
    stack *pilha;
    arc *arco;
+   int i;
 
    pilha = NULL;
    arco = NULL;
@@ -41,6 +42,55 @@ int main ( void ) {
    else
       printf ( "## FALHA\n" );
    printf ( "# Fim Teste 5\n" );
+
+   printf ( "# Teste 6 - STACK_print ( empty )\n" );
+   STACK_print ( pilha );
+   printf ( "# Fim Teste 6\n" );
+
+   printf ( "# Teste 7 - STACK_push ( pilha, 0, 1 )\n" );
+   STACK_push ( pilha, 0, 1 );
+   STACK_print ( pilha );
+   printf ( "# Fim Teste 7\n" );
+
+   printf ( "# Teste 8 - STACK_pop ( pilha )\n" );
+   arco = STACK_pop ( pilha );
+   if ( arco->u == 0 && arco->v == 1 )
+      printf ( "## SUCESSO\n" );
+   else
+      printf ( "## FALHA\n" );
+   STACK_print ( pilha );
+   printf ( "# Fim Teste 8\n" );
+
+   printf ( "# Teste 9 - 7 x STACK_push ( pilha, i, i + 1 )\n" );
+   for ( i = 0; i < 7; i++ )
+      STACK_push ( pilha, i, i + 1 );
+   STACK_print ( pilha );
+   printf ( "# Fim Teste 9\n" );
+
+   printf ( "# Teste 10 - 7 x STACK_pop ( pilha )\n" );
+   for ( i = 0; i < 7; i++ ) {
+      arco = STACK_pop ( pilha );
+      if ( i >= 5 ) {
+         if ( arco == NULL )
+            printf ( "## SUCESSO\n" );
+         else
+            printf ( "## FALHA\n" );
+      } else {
+         if ( arco->u == 4 - i && arco->v == 5 - i  )
+            printf ( "## SUCESSO\n" );
+         else
+            printf ( "## FALHA\n" );
+      }
+   }
+   printf ( "# Fim Teste 10\n" );
+
+   printf ( "# Teste 11 - STACK_end ( pilha )\n" );
+   pilha = STACK_end ( pilha );
+   if ( pilha == NULL )
+      printf ( "## SUCESSO\n" );
+   else
+      printf ( "## FALHA\n" );
+   printf ( "# Fim Teste 11\n" );
 
    return 0;
 }
